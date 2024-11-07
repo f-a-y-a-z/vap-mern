5296877db0c5e2b644894b1cbdbbed4c21c1bfeb
f-a-y-a-z <fayas0704@gmail.com>
2024-11-07 11:14:34 +0530

    Uploaded VAP Node Files

---
+++ index.mjs
@@ -0,0 +1,8 @@
+// const fs = require('fs');
+// fs.writeFileSync('hello.txt','Hello, world!');
+//const math = require('./math')
+import math from './math.js';
+console.log(math.add(7,4));
+console.log(math.sub(7,4));
+console.log(math.mul(7,4));
+console.log(math.div(7,4));
